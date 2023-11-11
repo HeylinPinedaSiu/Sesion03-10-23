d370fe56d0d82cdab7518a7400d1074f6b2caec6
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-11 17:18:54 -0500

    App.java

--- Sesion03-10-23/MetodosSobreCargados/src/App.java
+++ Sesion03-10-23/MetodosSobreCargados/src/App.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class App {
+    static Scanner lector = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        /*Metodos
+        Que no requieren datos de entrada ni devuelven datos de salida
+        Que no requieren datos de entrada pero devuelven datos de salida
+        Requieren datos de entrada pero no devuelven datos de salida
+        Requieren datos de entrada y devuelven datos de salida
+         ***********************************
+        Metodos sobrecargados (Sobrecarga de metodos)
+        Consiste en reutilizar el nombre de un metodo pero con distintas
+        listas de parametros de entrada*/
+        mostrarHora();
+        mostrarHora(2,15,36);
+        mostrarHora(2, 0);
+    }
+    
+    //Declaracion de metodos
+    //Version sin parametros de entrada
+    public static void mostrarHora(){   
+        System.out.print("Horas: ");
+        int hr = lector.nextInt();
+        System.out.print("Minutos: ");
+        int min = lector.nextInt();
+        System.out.print("Segundos: ");
+        int seg = lector.nextInt();
+        System.out.println("La hora es " + hr + ":" + min + ":" + seg);
+    }
+    
+    //Sobrecargar el metodo mostrarHora()
+    public static void mostrarHora(int hr, int min, int seg){
+        System.out.println("La hora es " + hr + ":" + min + ":" + seg);
+    }
+    
+    public static void mostrarHora(int hr, int min){
+        int seg = 46;
+        System.out.println("La hora es " + hr + ":" + min + ":" + seg);
     }
 }
